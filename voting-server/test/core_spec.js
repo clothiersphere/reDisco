e478c2c3f0d4fb2b3d5ac287d611f915b009dcf4
Spacemixup <xchrislee@gmail.com>
2016-03-18 15:24:03 -0700

    Add tally & vote

---
+++ voting-server/test/core_spec.js
@@ -0,0 +1,140 @@
+import {List, Map} from 'immutable';
+import {expect} from 'chai';
+
+import {setEntries, next, vote} from '../src/core';
+
+
+describe('application logic', () => {
+	
+	describe('setEntries', () => {
+		//loading in a collection of entries that will be voted on. 
+		// setEntries takes a previous state and a collection of entries and produces a state where the entries are loaded.
+		it ('adds the entries to the state', () => {
+			const state = Map();
+			const entries = List.of('Trainspotting', '28 Days Later');
+			const nextState = setEntries(state, entries);
+			expect(nextState).to.equal(Map({
+				entries: List.of('Trainspotting', '28 Days Later')
+			}));
+		});
+
+		it('converts to immutable', () => {
+			const state = Map(); 
+			const entries = ['Trainspotting', '28 Days Later'];
+			const nextState = setEntries(state, entries);
+			expect(nextState).to.equal(Map({
+				entries: List.of('Trainspotting', '28 Days Later')
+			}));
+		});
+	
+	});
+	
+
+	describe('next', () => {
+
+	// We can begin the voting by calling a function called next on a state that already has entries set. 
+	// That means, going from the first to the second of the state trees we designed.
+	// The function takes no additional arguments. It should create a vote Map on the state, 
+	// where the two first entries are included under the key pair. The entries under vote should no longer be in the entries List:
+		it('takes the next two entries under vote', () => {
+			const state = Map({
+				entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+			});
+			const nextState = next(state);
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later')
+				}),
+				entries: List.of('Sunshine')
+			}));
+		});
+
+		it('puts winner of current vote back to entries', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 4,
+						'28 Days Later': 2
+					})
+				}),
+				entries: List.of('Sunshine', 'Millions', '127 Hours')
+			});
+			const nextState = next(state);
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Sunshine', 'Millions')
+				}),
+				entries: List.of('127 Hours', 'Trainspottings')
+			}));
+		});
+		it('puts both from tied vote back to entries' () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 3, 
+						'28 Days Later': 3
+					})
+				}),
+				entries: List.of('Sunshine', 'Millions', '127 Hours')
+			});
+			const nextState = next(state);
+			expect(nextState.to.equal(Map({
+				vote: Map({
+					pair: List.of('Sunshine', 'Millions')
+				}),
+				entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
+			}));
+		});
+
+	});
+
+	describe('vote', () => {
+		
+		it('creates a tally for the voted entry', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later')
+				}),
+				entries: List()
+			});
+			const nextState = vote(state, 'Trainspotting');
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 1
+					})
+				}),
+				entries: List()
+			}));
+		});
+		
+		it ('adds to existing tally for the voted entry', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 3,
+						'28 Days Later': 2 
+					})
+				}),
+				entries: List()
+			});
+			const nextState = vote(state, 'Trainspotting');
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 4, 
+						'28 Days Later': 2
+					})
+				}),
+				entries: List()
+			}));
+		});
+
+	});
+
+});
