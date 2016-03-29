7f39596c3d40a589d7aee52593abd35b8e32f89c
Spacemixup <xchrislee@gmail.com>
2016-03-28 19:41:30 -0700

    Add mixin to Voting,Vote,Winner.jsx

--- voting-client/src/components/Vote.jsx
+++ voting-client/src/components/Vote.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
+	mixins: [PureRenderMixin],
 	getPair: function() {
 		return this.props.pair || [];
 	},
@@ -15,7 +17,7 @@ export default React.createClass({
 			{this.getPair().map(entry => 
 				<button key={entry}
 								disabled={this.isDisabled()}
-								onclick={() => this.props.vote(entry)}>
+								onClick={() => this.props.vote(entry)}>
 					<h1>{entry}</h1>
 					{this.hasVotedFor(entry) ? 
 						<div className="label">Voted</div> : 
